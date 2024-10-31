1a8bf61585750cb69f1240914d8e7b98a6a7259e
Kulemin Artem <moscowtema5@gmail.com>
2024-10-31 18:09:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int int1 = 15;
+	int int2 = 20;
+	int int3 = 30;
+
+	cout << boolalpha;
+
+	if (int1 == int3 && int2 != int3) {
+		cout << "operator == && ==" << "true" << endl;
+	}
+	else {
+		cout << "operator == && ==" << "false" << endl;
+	}
+	if (int1 >= int2 || int3 >= int2) {
+		cout << "operator == || ==" << "true" << endl;
+	}
+	else {
+		cout << "operator == || == " << "false" << endl;
+	}
+	return 0;
+}
